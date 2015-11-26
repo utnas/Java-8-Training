ffe38d1d887c4837d314ea784cad9602fc17c596
dnsewolo <nsewolo@gmail.com>
2015-11-26 10:11:18 -0500

    Renaming of test package

--- src/test/java/com/teksystems/training/helper/ItemMatcher.java
+++ src/test/java/com/teksystems/training/helper/ItemMatcher.java
@@ -9,13 +9,11 @@ public class ItemMatcher {
 
     public static Matcher<Item> hasProperties(final String name, final double price) {
         return new BaseMatcher<Item>() {
-            @Override
             public boolean matches(final Object input) {
                 Item item = (Item) input;
                 return item.getName().equals(name) && item.getBasePrice() == price;
             }
 
-            @Override
             public void describeTo(Description description) {
                 description.appendText(name + " or " + price + " does not match");
             }
